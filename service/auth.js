cf3e1e39e895a96dd3679c123c71aa147bdbcc6d
JeongTJ <dmswl022329@gmail.com>
2024-02-28 23:56:10 +0000

    feat: login resolve

--- service/auth.js
+++ service/auth.js
@@ -1,32 +1,31 @@
 require('dotenv').config();
 
 const { AuthorizationCode } = require ('simple-oauth2');
+const axios = require('axios');
 
-async function getClientInfo(code) {
+async function apiGetter(codee) {
 	try {
 		const client = new AuthorizationCode({
 			client: {
-				id: process.env.PUBLIC_ID,
-				secret: process.env.SECRET_KEY
+				id: process.env.LOGIN_42_API_UID,
+				secret: process.env.LOGIN_42_API_SECRET
 			},
-			auth : {
-				tokenHost: `${process.env.TOKENHOST}`
+			auth: {
+				tokenHost: process.env.TOKENHOST
 			}});
 		const accessToken = await client.getToken({
-			code: code,
+			code: codee,
 			redirect_uri: `${process.env.REDIRECT_URI}`
 		});
 		const apiUrl = `${process.env.TOKENHOST}/v2/me`;
 		const response = await axios.get(apiUrl, {
 			headers: {
 				Authorization: `Bearer ${accessToken.token.access_token}`
-			}
-		});
+			}});
 		return (response.data);
 	} catch (error){
-		return {
-			code: error.code,
-			message: error.message
-		};
+		throw error;
 	}
 }
+
+module.exports = apiGetter;
