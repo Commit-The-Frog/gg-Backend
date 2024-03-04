f95482afe74fac9ac7736f089c69f32f15f741b4
rillmo <maxman306@gmail.com>
2024-03-04 04:19:41 +0000

    added readBookOfTime function to check time confilct

--- repository/bookRepository.js
+++ repository/bookRepository.js
@@ -1,27 +1,43 @@
 const { Book } = require('../config/mongodbConfig');
 
 // CREATE book
-const addBook = async function (userId, start, end, date) {
+const createBook = async function (userId, start, end, date, type) {
 	var result = await Book.create({
 		user_id : userId,
 		start_time : start,
 		end_time : end,
-		date : date
+		date : date,
+		type : type
 	});
 	console.log(`### added book to DB : [userId:${result.user_id}, ${result.start_time}-${result.end_time}]`);
 	return result;
 };
 
-// FIND book by userId
-const findBookByUserId = async function (userId) {
+// READ book by userId
+const readBooksByUserId = async function (userId) {
 	var result = await Book.find({
 		user_id : userId
 	});
-	console.log(`### book searched from DB : [userId:${result.user_id}, ${result.start_time}-${result.end_time}]`);
+	console.log(`### book searched from DB : [count : ${result.length}]`);
 	return result;
 };
 
+// READ book by type and start/end time
+const readBookOfTime = async function (type, start, end, date) {
+	var result = await Book.find({
+		$and : [
+			{ type: type },
+			{ start_time : { $lt : end } },
+			{ end_time : { $gt : start }},
+			{ date : date }
+		]
+	});
+	console.log(`### book of time searched from DB : [count : ${result.length}]`);
+	return result;
+}
+
 module.exports = {
-	addBook,
-	findBookByUserId
+	createBook,
+	readBooksByUserId,
+	readBookOfTime
 };
