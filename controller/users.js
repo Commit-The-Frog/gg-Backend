e203726fd189d5d6fbef6efc6af923c4b46fcfb1
rillmo <maxman306@gmail.com>
2024-03-05 08:36:51 +0000

    build: integrated swagger

--- controller/users.js
+++ controller/users.js
@@ -1,7 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User 관련 API
+ */
+
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     summary: Get all users
+ *     tags: [Users]
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: 'respond with a resource'
+ */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
