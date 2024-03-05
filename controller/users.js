63d82d5aca3d1a22ba00a1f72aac3131deff21e0
minjacho42 <minjacho@student.42seoul.kr>
2024-03-05 15:37:54 +0900

    feat: userService made and router added for user

--- controller/users.js
+++ controller/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const userService = require('../service/userService.js');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res, next) {
+	try {
+		const usersInfo = await userService.getUsersInfo();
+		res.status(200).send(usersInfo);
+	} catch(error) {
+		res.status(error.status || 500).send(error.name || "Internal Server Error");
+	}
 });
+
+router.get('/:userId', async function (req, res, next) {
+	try {
+		const userInfo = await userService.getOneUserInfo(req.params.userId);
+		res.status(200).send(userInfo);
+	} catch(error) {
+		res.status(error.status || 500).send(error.name || "Internal Server Error");
+	}
+})
 
 module.exports = router;
