231cedd3b85007118958a2c73477fa93ab60a864
rillmo <maxman306@gmail.com>
2024-03-05 07:38:57 +0000

    feat: added GET routing methods

--- controller/book.js
+++ controller/book.js
@@ -2,20 +2,63 @@ var express = require('express');
 var router = express.Router();
 var bookService = require('../service/bookService');
 
-/* GET book list of user */
-router.get('/', async function(req, res, next) {
+/*	GET
+	/books/:userId/list?type&date : 해당 유저의 특정 타입, 특정 날짜의 예약 리스트 응답 */
+router.get('/:userId/list', async function(req, res, next) {
 	try {
-		var book = await bookService.findBookListOfUser(
-			req.query.userId
+		var bookList = await bookService.findBookListOfUserByTypeAndDate(
+			req.params.userId,
+			req.query.type,
+			req.query.date
 		);
-		res.status(200).send(book);
+		res.status(200).send(bookList);
 	}
 	catch (error) {
-		res.status(error.status).send(error.name);
+		res.status(error.status || 500).send(error.name || 'InternalServerError');
 	}
 });
 
-/* POST new book of user */
+/* 	GET
+	/books/:userId/history : 해당 유저의 모든 예약기록 응답 */
+router.get('/:userId/history', async function(req, res, next) {
+	try {
+		var bookList = await bookService.findBookListOfUser(
+			req.params.userId
+		);
+		res.status(200).send(bookList);
+	} catch (error) {
+		res.status(error.status || 500).send(error.name || 'InternalServerError');
+	}
+});
+
+/*	GET
+	/books?date : 해당 날짜의 모든 예약 목록 응답 */
+router.get('/', async function(req, res, next) {
+	try {
+		var bookList = await bookService.findBookListOfDate(
+			req.query.date
+		);
+		res.status(200).send(bookList);
+	} catch (error) {
+		res.status(error.status || 500).send(error.name || 'InternalServerError');
+	}
+})
+
+/* 	GET
+	/books/:bookId : 단일 예약 정보 응답 */
+router.get('/:bookId', async function (req, res, next) {
+	try {
+		var bookList = await bookService.findBookById(
+			req.params.bookId
+		);
+		res.status(200).send(bookList);
+	} catch (error) {
+		res.status(error.status || 500).send(error.name || 'InternalServerError');
+	}
+})
+
+/*	POST 
+	/books?userId&start&end&date&type : 예약 등록 */
 router.post('/', async function(req, res, next) {
 	try {
 		var book = await bookService.addBook(
@@ -27,7 +70,7 @@ router.post('/', async function(req, res, next) {
 		);
 		res.status(200).send(`successfully added book [${book._id}, ${book.start_time}-${book.end_time}]`);
 	} catch (error) {
-		res.status(error.status).send(error.name);
+		res.status(error.status || 500).send(error.name || 'InternalServerError');
 	}
 });
 
