6db99ac56639c48a1842073ab9e8477e41451b57
rillmo <maxman306@gmail.com>
2024-04-17 04:53:15 +0000

    feat: added enum types for device, report

---
+++ config/enum.js
@@ -0,0 +1,16 @@
+const DeviceStatus = {
+	MALFUNCTION: 0,
+	UNDER_REPAIR: 1,
+	NORMAL: 2
+};
+
+const ReportStatus = {
+	REPORTED: 0,
+	UNDER_REPAIR: 1,
+	REPAIRED:2
+};
+
+module.exports = {
+	DeviceStatus,
+	ReportStatus
+}
