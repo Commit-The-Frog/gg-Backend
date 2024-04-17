77f1faa6e35739141a4b5d5c62ed3330825018a8
rillmo <maxman306@gmail.com>
2024-04-17 04:37:51 +0000

    feat: added status to mongo schema

--- repository/reportPostRepository.js
+++ repository/reportPostRepository.js
@@ -1,6 +1,7 @@
 const { Report } = require('../config/mongodbConfig');
 
 // CREATE report
+// status : 0(접수됨) | 1(수리중) | 2(수리완료)
 const createReport = async (console_type, device, malf_type,
 	controller_btn_malf_list, etc_description) => {
 	try {
@@ -9,7 +10,8 @@ const createReport = async (console_type, device, malf_type,
 			device: device,
 			malf_type : malf_type,
 			controller_btn_malf_list : controller_btn_malf_list,
-			etc_description : etc_description
+			etc_description : etc_description,
+			status : 0
 		});
 		console.log('db insert success');
 		return result;
